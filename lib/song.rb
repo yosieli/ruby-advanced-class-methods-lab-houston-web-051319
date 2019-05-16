a1e95e116eb92abdb5fe33b65a9d68282a499427
feven <feventsegay6@gmail.com>
2019-05-16 00:59:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,44 @@ class Song
   def save
     self.class.all << self
   end
-
+ def self.create
+   song = self.new
+    song.save
+    song
+  end
+  def self.new_by_name(name)
+    song=self.create
+    song.name=name
+    song
+  end
+  def self.create_by_name(name)
+    song=self.create
+    song.name=name
+    song.save
+    song
+  end
+  def self.find_by_name(name)
+    song=self.all
+    song.find do |songs|
+      songs.name==name
+    end
+  end
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+    
+  end
+  def self.alphabetical
+    song=self.all
+    @@all.sort do|song| 
+    
+  end
+ end
+ def self.new_from_filename(song,artist_name)
+   @song=song
+   @artist_name=artist_name
+   
+   
+ end
+   
+    
 end
